cbcdd63fbd47b1d397a3dfc1762affac6a0b83b6
Vadim Sereda <flash.brovary@gmail.com>
2016-05-09 13:34:57 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 $(function(){
     var header = $('header');
-    var main = $('main');
+    var secondBlock = $('div');
 
     $(window).scroll(function() {
-
-        if($(this).scrollTop() > 1) {
+        if ($(this).scrollTop() > 0 && $(this).scrollTop() <= secondBlock.offset().top) {
             header.addClass('stickyHeader');
-            main.addClass('stickyMain');
-        } else {
-            header.removeClass('stickyHeader');
-            main.removeClass('stickyMain');
+            secondBlock.removeClass('stickyBlock');
         }
 
+        if ($(this).scrollTop() > secondBlock.offset().top) {
+            header.removeClass('stickyHeader');
+            secondBlock.addClass('stickyBlock');
+        }
+
+        if ($(this).scrollTop() == 0) {
+            header.removeClass('stickyHeader');
+            secondBlock.removeClass('stickyBlock');
+        }
     });
 });
 
+//$(window).scroll(function() {
+//    if ($(this).scrollTop() > 0 && $(this).scrollTop() <= secondBlock.offset().top) {
+//        header.addClass('stickyHeader');
+//        secondBlock.removeClass('stickyBlock');
+//    } else if ($(this).scrollTop() > secondBlock.offset().top) {
+//        header.removeClass('stickyHeader');
+//        secondBlock.addClass('stickyBlock');
+//    } else {
+//        header.removeClass('stickyHeader');
+//        secondBlock.removeClass('stickyBlock');
+//    }
+//});
